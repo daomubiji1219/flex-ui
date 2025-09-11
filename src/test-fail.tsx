b55fe718d54113a9b6e7b11b7a84d6a0e1bf4d8a
daomubiji1219 <3217724920@qq.com>
2025-09-11 16:29:07 +0800

    Keep calm and commit

--- src/test-fail.tsx
+++
@@ -1,9 +0,0 @@
-// This file should cause linting errors
-import React from 'react'
-
-const TestComponent = () => {
-  const unused = 'variable';
-  return <div>Test</div>
-}
-
-export default TestComponent
